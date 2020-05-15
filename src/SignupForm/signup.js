0ced45f20ef0f7eec0805457a08883849cf8671d
clintjeez <clintjeez@gmail.com>
2020-05-15 22:13:06 +0100

    validation update

--- src/SignupForm/signup.js
+++ src/SignupForm/signup.js
@@ -1,121 +1,190 @@
-import React,{ Component } from 'react';
+import React, { Component } from "react";
 
 // CSS Style
-import './signup.css';
+import "./signup.css";
+import errorIcon from '../images/icon-error.svg';
 
 class Signup extends React.Component {
-   
-    // Form fields data state 
-    state = {
-        fields: {},
-        errors: {},
-        buttonTxt: "Claim your free trial"
+  // Form fields data state
+  state = {
+    fields: {},
+    errors: {},
+    errorsImg: "",
+    buttonTxt: "Claim your free trial",
+  };
 
-    }
-
-    // Handle validation function binding form fields state
-    handleValidation(){
-        let fields = this.state.fields;
-        let errors = {};
-        let formIsValid = true;
+  // Handle validation function binding form fields state
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let errorsImg = {};
+    let formIsValid = true;
 
     // firstname validation
-    if(!fields["firstname"]){
+    if (!fields["firstname"]) {
+      formIsValid = false;
+      this.setState({ errorsImg: "errors"});
+      errors["firstname"] = "Firstname cannot be empty";
+    }
+    if (typeof fields["firstname"] !== "undefined") {
+      if (!fields["firstname"].match(/^[a-zA-Z]+$/)) {
         formIsValid = false;
-        errors["firstname"] = "Cannot be empty";
-    }
-    if(typeof fields["firstname"] !== "undefined"){
-        if(!fields["firstname"].match(/^[a-zA-Z]+$/)){
-            formIsValid = false;
-            errors["firstname"] = "only letters";
-        }
+        errors["firstname"] = "only letters";
+      }
     }
 
-    // Email validation 
-    if(!fields["email"]){
+    // lastname validation
+    if (!fields["lastname"]){
         formIsValid = false;
-        errors["email"] = "Cannot be empty";
-    }
-    if(typeof fields["email"] !== "undefined"){
-        let lastAtPos = fields["email"].lastIndexOf('@');
-        let lastDotPos = fields["email"].lastIndexOf('.');
+        errors["lastname"] = "Lastname cannot be empty"
+    };
 
-        if(!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].IndexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
-            formIsValid = false;
-            errors["email"] = "Email is not valid";
-        }
-    }
+    // password validation
+    if (!fields["password"]){
+        formIsValid = false;
+        errors["password"] = "Password cannot be empty"
+    };
 
-    this.setState({errors: errors});
+    // Email validation
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Looks like this not an email";
+    }
+    // if (typeof fields["email"] !== "undefined") {
+    //   let lastAtPos = fields["email"].lastIndexOf("@");
+    //   let lastDotPos = fields["email"].lastIndexOf(".");
+    //   if (
+    //     !(
+    //       lastAtPos < lastDotPos &&
+    //       lastAtPos > 0 &&
+    //       fields["email"].IndexOf("@@") == -1 &&
+    //       lastDotPos > 2 &&
+    //       fields["email"].length - lastDotPos > 2
+    //     )
+    //   ) {
+    //     formIsValid = false;
+    //     errors["email"] = "Email is not valid";
+    //   }
+    // }
+
+    this.setState({ errors: errors });
     return formIsValid;
-    }
-   
-// Handle form submit
-contactSubmit(e){
+  }
+
+  // Handle form submit
+  contactSubmit(e) {
     e.preventDefault();
 
-    if(this.handleValidation()){
-        this.setState({buttonTxt: "Form submitted"});
-    }else{
-        this.setState({buttonTxt: "Form has errors"});
+    if (this.handleValidation()) {
+      this.setState({ buttonTxt: "Form submitted" });
+     
+    } else {
+      this.setState({ buttonTxt: "Form has errors" });
     }
+  }
 
-}
-
-handleChange(field, e){
+  handleChange(field, e) {
     let fields = this.state.fields;
     fields[field] = e.target.value;
-    this.setState({fields});
-}
-    
+    this.setState({ fields });
+  }
 
-  
-    render(){
-        return(
-            <div className="Signup">
-                <div className="container">
-                    <div className="signup-wrapper">
-                        <div className="signup-txt">
-                            <h1>Learn to code by <br/> watching others</h1>
-                            <p>See how experienced developers solve problems in real-time.</p>
-                            <p>Watching scripted tutorials is great, but understanding 
-                            how developers think is invaluable.</p>
-                        </div>
-                        <div className="signup-form">
-                            <div className="form-header">
-                                <p className="header-txt">Try it free 7 days <span>then $20/mo. thereafter</span> </p>
-                            </div>
-                            <div className="form-wrapper">
-                                <form className="form-group" onSubmit={this.contactSubmit.bind(this)}>
-                                    <label for="firstname">
-                                    <input type="text" name="firstname" placeholder="First Name" 
-                                    value={this.state.fields["firstname"]} onChange={this.handleChange.bind(this, "firstname")} />
-                                    <span style={{color: "red"}}>{this.state.errors["firstname"]}</span>
-                                    </label>
-                                    <label for="lastname">
-                                    <input type="text" name="lastname" placeholder="Last Name" 
-                                    />
-                                    </label>
-                                    <label for="email">
-                                    <input type="email" name="email" placeholder="Email Address"
-                                    value={this.state.fields["email"]} onchange={this.handleChange.bind(this, "email")} />
-                                    <span>{this.state.errors["email"]}</span>
-                                    </label>
-                                    <label for="password">
-                                    <input type="password" name="password" placeholder="Password"
-                                     />
-                                    </label>
-                                    <button type="submit" name="submit">{this.state.buttonTxt}</button>
-                                </form>
-                                <p className="terms-txt">By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="Signup">
+        <div className="container">
+          <div className="signup-wrapper">
+            <div className="signup-txt">
+              <h1>
+                Learn to code by <br /> watching others
+              </h1>
+              <p>See how experienced developers solve problems in real-time.</p>
+              <p>
+                Watching scripted tutorials is great, but understanding how
+                developers think is invaluable.
+              </p>
             </div>
-        )
-    }
-    
+            <div className="signup-form">
+              <div className="form-header">
+                <p className="header-txt">
+                  Try it free 7 days <span>then $20/mo. thereafter</span>{" "}
+                </p>
+              </div>
+              <div className="form-wrapper">
+                <form
+                  className="form-group"
+                  onSubmit={this.contactSubmit.bind(this)}
+                >
+
+                  <label for="firstname">
+                    <input
+                      type="text"
+                      name="firstname"
+                      placeholder="First Name"
+                      className={this.state.errorsImg}
+                      value={this.state.fields["firstname"]}
+                      onChange={this.handleChange.bind(this, "firstname")}
+                    />
+                    <p className="errors-txt">
+                      {this.state.errors["firstname"]}
+                    </p>
+                  </label>
+
+                  <label for="lastname">
+                    <input
+                      type="text"
+                      name="lastname"
+                      placeholder="Last Name"
+                      className={this.state.errorsImg}
+                      value={this.state.fields["lastname"]}
+                      onChange={this.handleChange.bind(this, "lastname")}
+                    />
+                    <p className="errors-txt">
+                        {this.state.errors["lastname"]}
+                    </p>
+                  </label>
+
+                  <label for="email">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email Address"
+                      className={this.state.errorsImg}
+                      value={this.state.fields["email"]}
+                      onChange={this.handleChange.bind(this, "email")}
+                    />
+                    <p className="errors-txt">
+                        {this.state.errors["email"]}
+                    </p>
+                  </label>
+                  <label for="password">
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      className={this.state.errorsImg}
+                      value={this.state.fields["password"]}
+                      onChange={this.handleChange.bind(this, "password")}
+                    />
+                    <p className="errors-txt">
+                        {this.state.errors["password"]}
+                    </p>
+                  </label>
+                  <button type="submit" name="submit">
+                    {this.state.buttonTxt}
+                  </button>
+                </form>
+                <p className="terms-txt">
+                  By clicking the button, you are agreeing to our{" "}
+                  <span>Terms and Services</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Signup;
